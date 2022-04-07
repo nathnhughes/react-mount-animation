69dfff41d16e10eaaa5e54bba6b72050f410ae51
nathnhughes <nathan@silkstudio.co>
2022-04-07 13:46:49 +0100

    chore: convert from webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+
+/*
+
+
+
+
+
+
+
+
+*/
+
+const configBase = {
+  input: 'src/index.tsx',
+  external: id => !/^[./]/.test(id),
+}
+
+const configDev = {
+  ...configBase,
+  output: [
+    {
+      file: `dist/react-mount-animation.cjs.js`,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: `dist/react-mount-animation.js`,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  plugins: [esbuild()],
+}
+
+const configProd = {
+  ...configDev,
+  output: [
+    {
+      file: 'dist/react-mount-animation.cjs.min.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/react-mount-animation.min.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [esbuild({ minify: true })],
+}
+
+const configTS = {
+  ...configBase,
+  output: {
+    file: 'dist/index.d.ts',
+    format: 'es',
+  },
+  plugins: [dts()],
+}
+
+export default [configProd, configTS]
